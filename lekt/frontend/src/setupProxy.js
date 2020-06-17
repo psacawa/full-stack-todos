458f408664e8881f6ad03040ee48cf9022ed472f
Paweł Sacawa <psacawa@math.toronto.edu>
2020-06-17 02:00:04 -0400

    Add: Custom proxy from cra works with react-router

---
+++ lekt/frontend/src/setupProxy.js
@@ -0,0 +1,24 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = function(app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true
+    })
+  );
+  app.use(
+    "/accounts",
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true
+    })
+  );
+  app.use(
+    "/auth",
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true
+    })
+  );
+};
