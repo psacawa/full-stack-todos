76ebfb54b8a77b9514c844e5f9b02dc6734f987c
Paweł Sacawa <psacawa@math.toronto.edu>
2020-06-22 16:19:20 -0400

    Fix: todos endpoint automatically auths ...
    The owner attribute is supplied via TokenAuthentication

--- lekt/todos/models.py
+++ lekt/todos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.db.models.manager import BaseManager
+from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Todo(models.Model):
     """ Generic todo """
-    text = models.CharField (max_length = 1000)
-    author = models.CharField (max_length = 100, default = 'psacawa')
+    text = models.CharField(max_length=1000)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
--- lekt/todos/views.py
+++ lekt/todos/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.utils.decorators import method_decorator
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from rest_framework import fields
+from rest_framework.permissions import IsAuthenticated
 from .models import Todo
+#  from .utils import ReadWriteSerializerMixin
 
 class TodoSerializer(ModelSerializer):
+    owner = PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Todo
         fields = "__all__"
@@ -11,5 +16,17 @@ class TodoSerializer(ModelSerializer):
 class TodoViewSet(ModelViewSet):
     model = Todo
     serializer_class = TodoSerializer
-    queryset = Todo.objects.all()
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        """ Get only todos for the authed user. """
+        user = self.request.user
+        return Todo.objects.filter(owner=user)
+
+    def perform_create(self, serializer):
+        """ Fill in user from authentication data """
+        serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        """ Fill in user from authentication data """
+        serializer.save(owner=self.request.user)
