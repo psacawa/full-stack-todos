b18728ee0d745861e96ca04255f9e18dff6b3fb1
Paweł Sacawa <psacawa@math.toronto.edu>
2020-06-30 00:26:28 -0400

    Remove: Sqlite DB in development

--- lekt/lekt/settings/database.py
+++ lekt/lekt/settings/database.py
@@ -2,12 +2,13 @@
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASE_CONFIG = (
+    #  {
+    #      "ENGINE": "django.db.backends.sqlite3",
+    #      "NAME": join(BASE_DIR, "db.sqlite3"),
+    #  }
+    #  if DEBUG
+    #  else 
     {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": join(BASE_DIR, "db.sqlite3"),
-    }
-    if DEBUG
-    else {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "todos",
         "USER": "django",
