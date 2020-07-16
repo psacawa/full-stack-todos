cbfa326aa6e0bcd62d9c368a1eaed9b8e3974888
Paweł Sacawa <psacawa@math.toronto.edu>
2020-07-16 05:00:42 -0400

    Fix: Use of typesafe-actions automatic action...
    The type of the root action is automatically inferred from the Types
    interface in typesafe-actions and does not need to be specified on
    reducers.

--- lekt/frontend/src/store/types.d.ts
+++ lekt/frontend/src/store/types.d.ts
@@ -2,4 +2,7 @@ export {};
 declare module "typesafe-actions" {
   import { ActionType } from "typesafe-actions";
   export type RootAction = ActionType<typeof import("./actions").default>;
+  export interface Types {
+    RootAction: RootAction
+  }
 }
