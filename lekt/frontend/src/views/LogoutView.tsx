236673e90598e68ccb9d6cd35b6b9923efc5e540
Paweł Sacawa <psacawa@math.toronto.edu>
2020-08-10 19:38:37 -0400

    Fix: memory leak in LogoutView

--- lekt/frontend/src/views/LogoutView.tsx
+++ lekt/frontend/src/views/LogoutView.tsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Redirect } from "react-router";
-import {connect} from 'react-redux';
-import {logout} from 'store/actions';
+import { connect } from "react-redux";
+import { logout } from "store/actions";
 
 const dispatchProps = {
   logout: logout.request
-}
+};
 
-type Props = typeof dispatchProps
+type Props = typeof dispatchProps;
 
 const LogoutView = (props: Props) => {
-  props.logout()
+  useEffect(() => {
+    props.logout();
+  }, []);
   return (
     <>
       <Redirect to="/" />
     </>
   );
 };
-export default connect (null, dispatchProps)(LogoutView);
+export default connect(null, dispatchProps)(LogoutView);
